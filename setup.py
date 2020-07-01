b809789686dfbcdb36f8faa788100ca1561fbd27
dexter2206 <dexter2206@gmail.com>
2020-07-02 01:48:47 +0200

    Add entrypoint for random sampler

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnisolver",
-    entry_points={"console_scripts": ["omnisolver=omnisolver.cmd:main"]},
+    entry_points={
+        "console_scripts": ["omnisolver=omnisolver.cmd:main"],
+        "omnisolver": ["random = omnisolver.random"]
+    },
     install_requires=["dimod", "pluggy", "typing_extensions", "pyaml", "pandas"],
     tests_require=["pytest"],
     packages=find_packages(exclude=["tests"]),
-    package_data={"omnisolver": ["specifications/*.yml"]},
+    package_data={"omnisolver.random": ["random.yml"]},
 )
