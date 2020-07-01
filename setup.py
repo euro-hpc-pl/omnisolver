9499b7e538e70106ca43d022435e530f18a82e94
dexter2206 <dexter2206@gmail.com>
2020-07-02 00:37:00 +0200

    Add pluggy to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="omnisolver",
     entry_points={"console_scripts": ["omnisolver=omnisolver.cmd:main"]},
-    install_requires=["dimod", "typing_extensions", "pyaml", "pandas"],
+    install_requires=["dimod", "pluggy", "typing_extensions", "pyaml", "pandas"],
     tests_require=["pytest"],
     packages=find_packages(exclude=["tests"]),
     package_data={"omnisolver": ["specifications/*.yml"]},
