ab658a072765f850a79b36c042af52915e4ed0da
dexter2206 <dexter2206@gmail.com>
2020-11-02 01:50:16 +0100

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ setup(
         "console_scripts": ["omnisolver=omnisolver.cmd:main"],
         "omnisolver": ["random = omnisolver.random"],
     },
-    setup_requires=["setuptools-scm-4.1.2"],
+    setup_requires=["setuptools-scm==4.1.2"],
     install_requires=[
-        "dimod>-0.9.3",
-        "numpy>=1.17.0",
-        "pluggy=0.13.1",
-        "PyYAML=5.3.1",
-        "pandas=1.1.4"
+        "dimod==0.9.3",
+        "numpy==1.17.0",
+        "pluggy==0.13.1",
+        "PyYAML==5.3.1",
+        "pandas==1.1.4"
     ],
-    tests_require=["pytest=6.1.2", "pytest-mock=3.3.1"],
+    tests_require=["pytest==6.1.2", "pytest-mock==3.3.1"],
     packages=find_namespace_packages(exclude=["tests"]),
     package_data={"omnisolver.random": ["random.yml"]},
 )
