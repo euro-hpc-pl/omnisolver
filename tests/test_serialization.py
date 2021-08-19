68d21836413b84daff39b7a0913cfba29993e0f0
Konrad Jałowiecki <dexter2206@gmail.com>
2021-08-19 19:08:00 +0200

    Move tests outside the omnisolver package

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -23,7 +23,8 @@ def sorted_quadratic(quadratic: Dict[Any, float]):
 def test_sets_correct_vartype(vartype):
     """BQM loaded from coo should have correct vartype."""
     coo = make_coo(
-        """0 1 2.5
+        """
+        0 1 2.5
         1 1 -3
         2 0 4
         4 3 -1.2
