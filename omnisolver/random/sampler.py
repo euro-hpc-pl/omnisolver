4e5c66780c9b3007f3f54c8f2fdcd0bae3f15da8
Konrad Jałowiecki <dexter2206@gmail.com>
2023-06-18 00:03:13 +0200

    Improve typing support (#7)

--- omnisolver/random/sampler.py
+++ omnisolver/random/sampler.py
@@ -1,11 +1,12 @@
 """Simple implementation of dummy random sampler."""
 import random
 from functools import partial
+from typing import Dict, Hashable, cast
 
-import dimod
+from dimod import BQM, Sampler, SampleSet, vartypes  # type: ignore
 
 
-class RandomSampler(dimod.Sampler):
+class RandomSampler(Sampler):
     """Implementation of simple random-sampler.
 
     This sampler assigns randomly chosen value to each variable, either from
@@ -14,23 +15,25 @@ class RandomSampler(dimod.Sampler):
     """
 
     variable_samplers = {
-        dimod.vartypes.SPIN: lambda prob: int(random.random() > prob) * 2 - 1,
-        dimod.vartypes.BINARY: lambda prob: int(random.random() > prob),
+        vartypes.SPIN: lambda prob: int(random.random() > prob) * 2 - 1,
+        vartypes.BINARY: lambda prob: int(random.random() > prob),
     }
 
-    def __init__(self, prob):
+    def __init__(self, prob: float) -> None:
         self.prob = prob
 
-    def get_random_sample(self, bqm):
+    def get_random_sample(self, bqm: BQM) -> Dict[Hashable, int]:
         """Get random assignment of variables in given BQM."""
         get_random_value = partial(self.variable_samplers[bqm.vartype], prob=self.prob)
         return {variable: get_random_value() for variable in bqm.variables}
 
-    def sample(self, bqm, num_reads=1, **parameters):
+    def sample(self, bqm: BQM, num_reads=1, **parameters) -> SampleSet:
         samples = [self.get_random_sample(bqm) for _ in range(num_reads)]
         energies = [bqm.energy(sample) for sample in samples]
 
-        return dimod.SampleSet.from_samples(samples, energy=energies, vartype=bqm.vartype)
+        return cast(
+            SampleSet, SampleSet.from_samples(samples, energy=energies, vartype=bqm.vartype)
+        )
 
     @property
     def properties(self):
